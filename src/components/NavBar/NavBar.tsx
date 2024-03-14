abfc34f208bb3041e99b300c560e6596db7d02d0
pkbogovic <pkbogovic@gmail.com>
2024-03-14 18:29:33 +0100

    Added button to Navbar for accessing Add Quiz view

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
 import styles from "./NavBar.module.scss";
+import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+
+  const navigate = useNavigate();
+
+  const handleAddQuiz = () => {
+    navigate('/add-quiz');
+  }
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
@@ -10,6 +18,7 @@ const NavBar = () => {
             <Link to="/">Home</Link>
           </li>
         </ul>
+        <Button variant="contained" onClick={handleAddQuiz}>Add Quiz</Button>
       </div>
     </nav>
   );
